3c23d6c64e7d5af74af571657912222d71994ddb
Paladin333777 <romavolvyn@gmail.com>
2023-04-11 18:10:40 -0400

    Implement one round of the game.

--- script.js
+++ script.js
@@ -9,4 +9,32 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice())
+
+
+
+function playRound(playerSelection, computerSelection) {
+    
+    if(computerSelection == "Rock" && playerSelection == "rock") {
+        return "It's a tie."
+    } else if(computerSelection == "Paper" && playerSelection == "rock") {
+        return "You lost. Paper is stronger than Rock."
+    } else if(computerSelection == "Scissors" && playerSelection == "rock"){
+        return "You Won!"
+    } else if(computerSelection == "Rock" && playerSelection == "paper") {
+        return "You Won!"
+    } else if(computerSelection == "Paper" && playerSelection == "paper") {
+        return "It's a tie."
+    } else if(computerSelection == "Scissors" && playerSelection == "paper"){
+        return "You lost. Scissors is stronger than Paper."
+    } else if(computerSelection == "Rock" && playerSelection == "scissors") {
+        return "You lost. Rock is stronger than Scissors."
+    } else if(computerSelection == "Paper" && playerSelection == "scissors") {
+        return "You Won!"
+    } else if(computerSelection == "Scissors" && playerSelection == "scissors"){
+        return "It's a tie."
+    }
+}   
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Rock, Paper, Scissors").toLowerCase();
+
+console.log(playRound(playerSelection, computerSelection))
